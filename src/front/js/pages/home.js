3a13d4906b51dc86b27910fc6d8cde003f13b22d
Scott B Bowman <145110529+Arcarius41@users.noreply.github.com>
2024-02-13 02:02:58 +0000

    added the cards and hero section to home page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,49 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>WHAT??!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="container mt-5">
+			{/* Hero Section */}
+			<div className="row">
+				<div className="col-12">
+					<div className="text-center">
+						<h1>HERO SECTION</h1>
+						<p>
+							<img src={rigoImageUrl} className="img-fluid" alt="Responsive image" />
+						</p>
+					</div>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+			
+			{/* Testimonial Cards Section */}
+			<div className="row">
+				<div className="col-md-4 my-3">
+					<div className="card">
+						<img src="..." className="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-4 my-3">
+					<div className="card">
+						<img src="..." className="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-4 my-3">
+					<div className="card">
+						<img src="..." className="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
