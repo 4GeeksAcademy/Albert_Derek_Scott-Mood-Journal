bc3c9fc5eee8240fb963d1b39cf201f832ad8b97
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2024-02-13 01:40:16 +0000

    second commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,16 +5,16 @@ import Logo from "../../img/SerenityScribe.png";
 export const Navbar = () => {
 	return (
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">
+			<a class="navbar-brand" href="/">
 				<img src={Logo} width="30" height="30" alt="Logo"/>
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="/">Home</a>
+				<ul class="navbar-nav ms-auto">
+				<li class="nav-item">
+						<a class="nav-link" href="/login/">Register</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="/login/">Login</a>
